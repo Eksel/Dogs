b293ceb2883346bc5af2fad3e1cdb61908dc0bca
Krzysztof <krzysiu.heksel@gmail.com>
2023-12-17 00:41:06 +0100

    try try again

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ app.use((res,req,next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+    
+    
     console.log(req.path,req.method)
     next()
 })
