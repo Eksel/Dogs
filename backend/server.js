d8adbe720f1ecb3c98bd0109968740bd58609bee
Krzysztof <krzysiu.heksel@gmail.com>
2023-12-17 11:20:40 +0100

    last try

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin: 'https://papito.onrender.com/dogs/'
   }));
 app.use(express.json())
-app.use((res,req,next) => {
+app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
